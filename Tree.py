7cd3199eb2e94330c90715c827d7608cff5499b9
Sebastian Kroesche <ga69qil@stu31.lis.ei.tum.de>
2016-04-02 10:20:07 +0200

    adds clb_dependencies - a tool to search for the dependencies of the output of a combinational logic block

---
+++ Tree.py
@@ -0,0 +1,18 @@
+class Tree(object):
+    "Generic tree node."
+    def __init__(self, name='root', children=None):
+        self.name = name
+        self.children = []
+        if children is not None:
+            for child in children:
+                self.add_child(child)
+    def __repr__(self):
+        return self.name
+    def add_child(self, node):
+        assert isinstance(node, Tree)
+        self.children.append(node)
+    def has_children(self):
+        if self.children != []:
+            return True
+        else:
+            return False
